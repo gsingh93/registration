1fa86500a08de11e473e3b43b596a7edbc118a9c
Gulshan Singh <gsingh2011@gmail.com>
2016-06-04 21:29:24 -0700

    Added camp parse project

---
+++ parse/CampRegistration/cloud/main.js
@@ -0,0 +1,102 @@
+Parse.Cloud.afterSave("Camper", function(request) {
+    var name = request.object.get("name");
+
+    var to = request.object.get("primaryEmail");
+    sendEmail(to, name);
+
+    to = request.object.get("secondaryEmail");
+    if (to != '') {
+        sendEmail(to, name);
+    }
+});
+
+function sendEmail(to, name) {
+  Parse.Config.get().then(function(config) {
+    console.log("Sending email to " + to);
+    var sendgrid_password = config.get("sendgrid_password");
+
+    var sendgrid = require("sendgrid");
+    sendgrid.initialize("gsingh93", sendgrid_password);
+    sendgrid.sendEmail({
+        to: to,
+        from: "camp_registration@michigangurudwara.com",
+        subject: "Registration confirmation",
+        text: "This email confirms that " + name + " has been successfully registered for this year's youth camp.\n\nPlease do not respond to this email."
+    }).then(function(httpResponse) {
+        console.log("Email sent successfully");
+    },function(httpResponse) {
+        console.error(httpResponse);
+    });
+  });
+}
+
+Parse.Cloud.job("summaryEmail", function(request, status) {
+    Parse.Config.get().then(function(config) {
+        var send_summary = config.get("send_summary");
+        var owner_email = config.get("owner_email");
+        var sendgrid_password = config.get("sendgrid_password");
+
+        sendSummaryEmail(send_summary, owner_email, status, sendgrid_password);
+    }, function(error) {
+        status.message('Retrieving config failed, falling back to cached config.');
+        var config = Parse.Config.current();
+        var send_summary = config.get("send_summary");
+        var owner_email = config.get("owner_email");
+
+        sendSummaryEmail(send_summary, owner_email, status);
+    });
+});
+
+function sendSummaryEmail(send_summary, owner_email, status, sendgrid_password) {
+    if (send_summary) {
+        status.message("Sending summary email to " + owner_email);
+    } else {
+        status.success("Summary email disabled, not sending email");
+        return;
+    }
+
+    getNumRegistrations(function(total_results, recent_results) {
+        if (recent_results == 0) {
+            status.success("No recent results, skipping email");
+            return;
+        }
+
+        var sendgrid = require("sendgrid");
+        sendgrid.initialize("gsingh93", sendgrid_password);
+        sendgrid.sendEmail({
+            to: owner_email,
+            from: "camp_registration@michigangurudwara.com",
+            subject: "Registration summary",
+            text: recent_results + " registrations have been received in the last 24 hours, and "
+                + total_results + " registrations have been received total."
+        }).then(function(httpResponse) {
+            status.success("Email sent successfully");
+        },function(httpResponse) {
+            status.error(httpResponse);
+        });
+    });
+}
+
+function getNumRegistrations(callback) {
+    var Student = Parse.Object.extend("Camp");
+    var query = new Parse.Query(Student);
+    query.count({
+        success: function(total_results) {
+            var now = new Date();
+            var day = 1 * 24 * 60 * 60 * 1000;
+            var yesterday = new Date(now.getTime() - day);
+            query.greaterThanOrEqualTo("createdAt", yesterday);
+            query.count({
+                success: function(recent_results) {
+                    callback(total_results, recent_results);
+                },
+                error: logError
+            });
+        },
+        error: logError
+    });
+}
+
+function logError(error) {
+    console.log('Error: (' + error.code + ') ' + error.message);
+}
