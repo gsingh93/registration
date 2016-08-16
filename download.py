b56cc14ae16aa24cf615780336a456219e5ddfee
Gulshan Singh <gsingh2011@gmail.com>
2016-08-15 23:25:29 -0700

    Added downloading script

---
+++ download.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python2
+
+import csv
+import json
+import requests
+import yaml
+
+def main():
+    with open('config.yaml') as f:
+        config = yaml.safe_load(f)
+
+    url = 'https://punjabi-class-registration.firebaseio.com/students.json?auth=' + config['FIREBASE_SECRET']
+    r = requests.get(url)
+    results = json.loads(r.content)
+
+    print 'Creating CSV'
+    f = csv.writer(open("students.csv", "wb+"))
+    column_names = [
+        'Student Name',
+        'Birthday',
+        'Gender',
+        'Grade',
+        'Father',
+        'Mother',
+        'Address',
+        'Primary Email',
+        'Secondary Email',
+        'Phone Number',
+        'Amount',
+        'Payment Type',
+    ]
+
+    f.writerow(column_names)
+
+    for res in results.itervalues():
+        amount = ''
+        payment = ''
+        f.writerow([
+            res['name'],
+            res['birthday'],
+            res['gender'],
+            res['class'],
+            res['father'],
+            res['mother'],
+            res['address'],
+            res['primaryEmail'],
+            res['secondaryEmail'],
+            res['homePhoneNumber'],
+            amount,
+            payment,
+        ])
+
+if __name__ == '__main__':
+    main()
